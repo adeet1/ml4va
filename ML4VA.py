c89ecc26b85112d2c9c88baee21c561fbee28329
Adeet Patel <adeet.patel@gmail.com>
2020-10-18 18:19:55 -0400

    Try creating pipeline (but this code throws an error)

--- ML4VA.py
+++ ML4VA.py
@@ -14,49 +14,80 @@ import IPython
 import IPython.display
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
 
-df = pd.read_csv("Virginia_Crashes.csv")
+#%%
 
-print(df.head())
-print(df['Rd_Type'].value_counts())
-print(df.shape)
-print(df["Crash_Dt"].value_counts())
+df0 = pd.read_csv("Virginia_Crashes.csv")
+
+#%%
+
+# Drop redundant columns
+df = df0.drop(["OBJECTID", "Document_Nbr", "Rte_Nm", "Local_Case_Cd", "DIAGRAM", "Node_Info", "X", "Y"], axis = 1)
+
+#%%
+
+features = {
+        "ordinal" : ["Time_Slicing", "Speed_Notspeed", "Belted_Unbelted", "Alcohol_Notalcohol", "Crash_Severity"],
+        "nominal" : ["Weather_Condition", "First_Harmful_Event_of_Entire_C", "Collision_Type", "FAC", "FUN", "Light_Condition", "VDOT_District", "Ownership_Used", "Crash_Event_Type_Dsc", "Roadway_Surface_Cond"],
+        "numerical": ["Rns_Mp", "K_People", "A_People", "B_People", "C_People", "LATITUDE", "LONGITUDE", "VSP", "SYSTEM", "OWNERSHIP", "Carspeedlimit", "Crash_Military_Tm", "Driverage"]
+        }
+
+pipeline_num = Pipeline([("imputer", SimpleImputer(strategy = "median")), ("scaler", StandardScaler())])
+
+pipeline = ColumnTransformer([("numerical", pipeline_num, features["numerical"]), ("ordinal", OrdinalEncoder(), features["ordinal"]), ("nominal", OneHotEncoder(), features["nominal"])])
+
+df_tr = pipeline.fit_transform(df)
+
+#%%
+
+# Drop: Crash_Dt, CRASH_YEAR
+# We could feature engineer Light_Condition
+# We could group regions (Physical_Juris) together into Northern Virginia, Central Virginia, urban, rural, etc.
+# Need to see if every city in Virginia has a corresponding VDOT_District
+# Need to choose between one of: Physical_Juris, Plan_District, VDOT_District
+# Need to choose between one of: FUN, Rte_Category_Cd
+
+#print(df["Crash_Dt"].value_counts())
 print("")
-print(df["Time_Slicing"].value_counts())
+#print(df["Time_Slicing"].value_counts())
 print("")
-print(df["Weather_Condition"].value_counts())
+#print(df["Weather_Condition"].value_counts())
 print("")
-print(df["First_Harmful_Event_of_Entire_C"].value_counts())
+#print(df["First_Harmful_Event_of_Entire_C"].value_counts())
 print("")
-print(df["Speed_Notspeed"].value_counts())
+#print(df["Speed_Notspeed"].value_counts())
 print("")
-print(df["Belted_Unbelted"].value_counts())
+#print(df["Belted_Unbelted"].value_counts())
 print("")
-print(df["Alcohol_Notalcohol"].value_counts())
+#print(df["Alcohol_Notalcohol"].value_counts())
 print("")
 print(df["Rd_Type"].value_counts())
 print("")
-print(df["Collision_Type"].value_counts())
-print("")
+#print(df["Collision_Type"].value_counts())
+#print("")
 print(df["Vehicle_Body_Type_Cd"].value_counts())
 print("")
 print(df["Driver_Action_Type_Cd"].value_counts())
 print("")
-print(df["Crash_Severity"].value_counts())
+#print(df["Crash_Severity"].value_counts())
 print("")
 print(df["CRASH_YEAR"].value_counts())
 print("")
-print(df["FAC"].value_counts())
+#print(df["FAC"].value_counts())
 print("")
-print(df["FUN"].value_counts())
+#print(df["FUN"].value_counts())
 print("")
-print(df["Light_Condition"].value_counts())
+#print(df["Light_Condition"].value_counts())
 print("")
-print(df["VDOT_District"].value_counts())
+#print(df["VDOT_District"].value_counts())
 print("")
-print(df["Ownership_Used"].value_counts())
+#print(df["Ownership_Used"].value_counts())
 print("")
-print(df["Physical_Juris"].value_counts())
+#print(df["Physical_Juris"].value_counts())
 print("")
 print(df["Plan_District"].value_counts())
 print("")
@@ -77,4 +108,6 @@ print("")
 print(df["Passgen"].value_counts())
 print("")
 print(df["Passinjurytype"].value_counts())
+
+#%%
 
